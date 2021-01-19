1115cbb1ebead24eba870c5a731fb16f3af93e2a
Nian <457920700@qq.com>
2021-01-19 11:47:17 +0800

    初始化项目

--- apweb_v21/src/components/DemoHeader.vue
+++ apweb_v21/src/components/DemoHeader.vue
@@ -1,34 +1,99 @@
 <template>
-  <div>
-    <a-row>
-      <a-col :span="6">
-        <img alt="Vue logo" src="../assets/logo.png">
-      </a-col>
-      <a-col :span="18">
-        <a-menu v-model="current" mode="horizontal">
-          <a-sub-menu>
-            <span slot="title" class="submenu-title-wrapper">
-            <a-icon type="setting"/>Navigation Three - Submenu</span>
-            <a-menu-item-group title="Item 1">
-              <a-menu-item key="setting:1">
-                Option 1
-              </a-menu-item>
-              <a-menu-item key="setting:2">
-                Option 2
-              </a-menu-item>
-            </a-menu-item-group>
-            <a-menu-item-group title="Item 2">
-              <a-menu-item key="setting:3">
-                Option 3
-              </a-menu-item>
-              <a-menu-item key="setting:4">
-                Option 4
-              </a-menu-item>
-            </a-menu-item-group>
-          </a-sub-menu>
-        </a-menu>
-      </a-col>
-    </a-row>
+  <div id="components-layout-demo-basic">
+    <a-layout>
+      <a-layout-header>
+        <a-row>
+          <!-- 左边的LOGO -->
+          <a-col :span="4">
+            <img alt="Vue logo" class="apweb-logo" src="../assets/logo.png">
+          </a-col>
+          <!-- 导航列表-->
+          <a-col :span="20">
+            <a-menu v-model="current" mode="horizontal">
+              <a-menu-item key="mail"> <a-icon type="mail" />HOME</a-menu-item>
+              <a-sub-menu>
+                <span slot="title" class="submenu-title-wrapper"><a-icon type="setting"/>PARTICIPATING</span>
+                <a-menu-item-group title="Item 1">
+                  <a-menu-item key="setting:1">
+                    Option 1
+                  </a-menu-item>
+                  <a-menu-item key="setting:2">
+                    Option 2
+                  </a-menu-item>
+                </a-menu-item-group>
+                <a-menu-item-group title="Item 2">
+                  <a-menu-item key="setting:3">
+                    Option 3
+                  </a-menu-item>
+                  <a-menu-item key="setting:4">
+                    Option 4
+                  </a-menu-item>
+                </a-menu-item-group>
+              </a-sub-menu>
+              <a-sub-menu>
+                <span slot="title" class="submenu-title-wrapper"><a-icon type="setting"/>PROGRAM</span>
+                <a-menu-item-group title="Item 1">
+                  <a-menu-item key="setting:1">
+                    Option 1
+                  </a-menu-item>
+                  <a-menu-item key="setting:2">
+                    Option 2
+                  </a-menu-item>
+                </a-menu-item-group>
+                <a-menu-item-group title="Item 2">
+                  <a-menu-item key="setting:3">
+                    Option 3
+                  </a-menu-item>
+                  <a-menu-item key="setting:4">
+                    Option 4
+                  </a-menu-item>
+                </a-menu-item-group>
+              </a-sub-menu>
+              <a-sub-menu>
+                <span slot="title" class="submenu-title-wrapper"><a-icon type="setting"/>SUBMISSION</span>
+                <a-menu-item-group title="Item 1">
+                  <a-menu-item key="setting:1">
+                    Option 1
+                  </a-menu-item>
+                  <a-menu-item key="setting:2">
+                    Option 2
+                  </a-menu-item>
+                </a-menu-item-group>
+                <a-menu-item-group title="Item 2">
+                  <a-menu-item key="setting:3">
+                    Option 3
+                  </a-menu-item>
+                  <a-menu-item key="setting:4">
+                    Option 4
+                  </a-menu-item>
+                </a-menu-item-group>
+              </a-sub-menu>
+              <a-sub-menu>
+                <span slot="title" class="submenu-title-wrapper"><a-icon type="setting"/>ORGANIZATION</span>
+                <a-menu-item-group title="Item 1">
+                  <a-menu-item key="setting:1">
+                    Option 1
+                  </a-menu-item>
+                  <a-menu-item key="setting:2">
+                    Option 2
+                  </a-menu-item>
+                </a-menu-item-group>
+                <a-menu-item-group title="Item 2">
+                  <a-menu-item key="setting:3">
+                    Option 3
+                  </a-menu-item>
+                  <a-menu-item key="setting:4">
+                    Option 4
+                  </a-menu-item>
+                </a-menu-item-group>
+              </a-sub-menu>
+              <a-menu-item key="mail"> <a-icon type="mail" />WORKSHOPS</a-menu-item>
+              <a-menu-item key="mail"> <a-icon type="mail" />PREVIOUS CONFERENCES</a-menu-item>
+            </a-menu>
+          </a-col>
+        </a-row>
+      </a-layout-header>
+    </a-layout>
   </div>
 </template>
 
@@ -47,5 +112,7 @@ export default {
 </script>
 
 <style scoped>
-
+.apweb-logo{
+  width: 100px;
+}
 </style>
