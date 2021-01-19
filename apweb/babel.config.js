d3b19b3616a986fc04c9fa94026d030641c35f00
Nian <457920700@qq.com>
2021-01-19 10:04:04 +0800

    初始化项目

--- apweb/babel.config.js
+++ apweb/babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
     '@vue/app'
-  ]
+  ],
+    // 新增
+    plugins: [
+        [
+            'import',
+            { libraryName: 'ant-design-vue', libraryDirectory: 'es', style: true }
+        ]
+    ]
 }
